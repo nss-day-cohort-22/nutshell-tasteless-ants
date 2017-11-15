21bd78b833d3316df706ca4aa531f2236cc40072
JPage4 <jesse.page@me.com>
2017-11-15 14:50:38 -0600

    fixed lint errors TOTALLY DONE NOW

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,5 +1,4 @@
 module.exports = function(grunt) {
-  
         // Project configuration.
         grunt.initConfig({
           pkg: grunt.file.readJSON("package.json"),
@@ -12,12 +11,12 @@ module.exports = function(grunt) {
                 },
               },
             },
-  
+
           uglify: {
             options: {
               banner: "/*! <%= pkg.name %> <%= grunt.template.today(\"yyyy-mm-dd\") %> */\n"
             },
-  
+
           build: {
               files: [{
                   expand: true,
@@ -40,18 +39,17 @@ module.exports = function(grunt) {
                 debug: true,
             }
         },
-        },
         eslint: {
         src: ["scripts/**/*.js", "!node_modules/**?*.js"]
       },
-    });
+    }
+  )
+  // Load the plugin that provides the "uglify" task.
+  grunt.loadNpmTasks("grunt-contrib-uglify");
+  grunt.loadNpmTasks("grunt-contrib-watch");
+  grunt.loadNpmTasks("gruntify-eslint");
+  grunt.loadNpmTasks("grunt-browserify");
+  // Default task(s).
+  grunt.registerTask("default", ["eslint", "browserify", "watch"]);
+}
 
-      // Load the plugin that provides the "uglify" task.
-      grunt.loadNpmTasks("grunt-contrib-uglify");
-      grunt.loadNpmTasks("grunt-contrib-watch");
-      grunt.loadNpmTasks("gruntify-eslint");
-      grunt.loadNpmTasks("grunt-browserify");
-
-      // Default task(s).
-      grunt.registerTask("default", ["eslint", "browserify", "watch"]);
-    };
